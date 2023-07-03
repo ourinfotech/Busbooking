6e174886c79d24f95c46451171683691734c21db
bgdit <bgdit@d5.bhavishgraphics.com>
2023-07-03 11:56:46 +0530

    03072023 all change insert.

---
+++ src/main/java/com/BusBooking/demo/controller/UserPage.java
@@ -0,0 +1,16 @@
+package com.BusBooking.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/busbooking")
+public class UserPage {
+	
+	@GetMapping("/login")
+	public String user() {
+		return "index";
+	}
+
+}
--- src/main/webapp/WEB-INF/view/index.jsp
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -2,9 +2,10 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
+ <link rel="stylesheet" href="css/login.css">
 <title>Insert title here</title>
 </head>
 <body>
-
+hi
 </body>
 </html>
